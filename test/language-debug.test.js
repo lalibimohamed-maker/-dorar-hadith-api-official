911ec5b454461a799455331b488016a8c2727c4e
lalibimohamed-maker <lalibi.mohamed@yahoo.com>
2026-08-21 23:03:08 +0100

    test: remove temporary language detection debug

--- test/language-debug.test.js
+++
@@ -1,7 +0,0 @@
-import test from "node:test";
-import assert from "node:assert/strict";
-import { detectLanguage } from "../src/language.js";
-
-test("debug French detection", () => {
-  assert.equal(detectLanguage("Quelle est la source de ce hadith ?").code, "fr");
-});
